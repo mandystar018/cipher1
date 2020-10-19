9d9f48b17337ae73aa26aacb563fbc46a640fc32
Amanda Otero <mandystar018@gmail.com>
2020-10-19 16:41:15 -0700

    Add fuctions in line 6 and 14
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-alert("Hello World");
+$(document).ready(function(){
+
+  const sentence = (prompt('Write a sentence'));
+  console.log(sentence);
+
+function newString(sentence){
+  const last = sentence.charAt(sentence.length-1).toUpperCase();
+  const first = sentence.charAt(0).toUpperCase();
+  const result = first + last;
+  return result;
+}
+let reverse = newString(sentence);
+
+function order(reverse) {
+  const newFirst = first[0];
+  const newLast = last[1];
+  const together = newFirst + newLast;
+  return together;
+}
+alert(newString(sentence));
+alert(order(reverse));
+});
